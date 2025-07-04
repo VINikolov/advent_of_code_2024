2131e6e79032045801b13c4e4ba46647cd16161a
VINikolov <v.nikolov.95@gmail.com>
2025-07-04 17:44:25 +0300

    feat: day 7 part 2

--- src/day_seven_solution.rs
+++ src/day_seven_solution.rs
@@ -29,17 +29,17 @@ fn solve_equation(idx: usize, current_value: i64, values: &Vec<i64>, target: &i6
         return false;
     }
 
-    return solve_equation(idx + 1, current_value + values[idx + 1], values, target)
-        || solve_equation(idx + 1, current_value * values[idx + 1], values, target);
+    let idx = idx + 1;
+
+    return solve_equation(idx, current_value + values[idx], values, target)
+        || solve_equation(idx, current_value * values[idx], values, target);
 }
 
 fn check_equation((result, values): &(i64, Vec<i64>)) -> bool {
     return solve_equation(0, values[0], values, result);
 }
 
-fn solve_part_one(file: &str) -> i64 {
-    let input = parse_input(file);
-
+fn solve_part_one(input: &Vec<(i64, Vec<i64>)>) -> i64 {
     return input
         .iter()
         .filter(|line| check_equation(*line))
@@ -47,8 +47,51 @@ fn solve_part_one(file: &str) -> i64 {
         .sum();
 }
 
+fn concat_numbers(a: i64, b: i64) -> i64 {
+    return a as i64 * 10i64.pow(b.ilog10() + 1) + b as i64;
+}
+
+fn solve_equation_with_number_merge(
+    idx: usize,
+    current_value: i64,
+    values: &Vec<i64>,
+    target: &i64,
+) -> bool {
+    if idx + 1 >= values.len() {
+        return current_value == *target;
+    }
+
+    if current_value > *target {
+        return false;
+    }
+
+    let idx = idx + 1;
+
+    return solve_equation_with_number_merge(idx, current_value + values[idx], values, target)
+        || solve_equation_with_number_merge(idx, current_value * values[idx], values, target)
+        || solve_equation_with_number_merge(
+            idx,
+            concat_numbers(current_value, values[idx]),
+            values,
+            target,
+        );
+}
+
+fn check_equation_with_number_merge((result, values): &(i64, Vec<i64>)) -> bool {
+    return solve_equation_with_number_merge(0, values[0], values, result);
+}
+
+fn solve_part_two(input: &Vec<(i64, Vec<i64>)>) -> i64 {
+    return input
+        .iter()
+        .filter(|line| check_equation_with_number_merge(*line))
+        .map(|line| line.0)
+        .sum();
+}
+
 pub fn solve_day_seven() -> (i64, i64) {
     let file = fs::read_to_string("./src/input_files/day_seven.txt").unwrap();
+    let input = parse_input(&file);
 
-    return (solve_part_one(&file), 0);
+    return (solve_part_one(&input), solve_part_two(&input));
 }
