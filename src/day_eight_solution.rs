7a1d3b5678d9a0665581c538f3304fe804d8912c
VINikolov <v.nikolov.95@gmail.com>
2025-07-16 11:42:37 +0300

    feat: WIP day eight

---
+++ src/day_eight_solution.rs
@@ -0,0 +1,7 @@
+use std::fs;
+
+pub fn solve_day_eight() -> (i32, i32) {
+    let file = fs::read_to_string("./src/input_files/day_eight.txt").unwrap();
+
+    return (0, 0);
+}
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@
 // mod day_four_solution;
 // mod day_five_solution;
 // mod day_six_solution;
-mod day_seven_solution;
+// mod day_seven_solution;
+mod day_eight_solution;
 
 // use crate::day_one_solution::solve_day_one;
 // use crate::day_two_solution::solve_day_two;
@@ -12,10 +13,11 @@ mod day_seven_solution;
 // use crate::day_four_solution::solve_day_four;
 // use crate::day_five_solution::solve_day_five;
 // use crate::day_six_solution::solve_day_six;
-use crate::day_seven_solution::solve_day_seven;
+// use crate::day_seven_solution::solve_day_seven;
+use crate::day_eight_solution::solve_day_eight;
 
 fn main() {
-    let (answer_one, answer_two) = solve_day_seven();
+    let (answer_one, answer_two) = solve_day_eight();
 
     println!("Solution for problem one: {answer_one}");
     println!("Solution for problem two: {answer_two}");
